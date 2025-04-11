836f60742c220b172665dfd93d6fe98eed24234a
Victor Pena <victorcpena1@gmail.com>
2025-04-11 17:26:11 -0300

    teste

--- app.py
+++ app.py
@@ -332,4 +332,7 @@ def exibir_detalhes_anuncio(selected_idx, candidato):
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=10000)
+
+
+    
